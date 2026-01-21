c6e13f8675910227a64ba7b90e7495d838762209
hamza emin <hamzaemin58@gmail.com>
2026-01-22 02:55:36 +0300

    Add files via upload

---
+++ mad_lib_game.c
@@ -0,0 +1,53 @@
+// Mad Libs Game in C
+// Author: Hamza Emin
+// Day 2 of learning C
+// Description: A simple text-based game that practices string input using fgets
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char noun[50] = "";
+    char verb[50] = "";
+    char adjective1[50] = ""; 
+    char adjective2[50] = ""; 
+    char adjective3[50] = ""; 
+
+    printf("Enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) -1] = '\0';
+
+    printf("Enter a noun(animal or person); ");
+    fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) -1] = '\0';
+
+    
+    printf("enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) -1] = '\0';
+    
+    printf("enter a verb (ending w/ -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) -1] = '\0';
+    
+    printf("enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) -1] = '\0';
+    
+
+    // Print the final story using user inputs
+
+    printf("\n--- Your Story ---\n");
+    printf("Today i went to a %s zoo\n", adjective1);
+    printf("In an exhibit,I saw a %s\n", noun);
+    printf("%s was %s and %s !\n", noun, adjective2,verb);
+    printf("I was %s\n", adjective3);
+   
+
+
+    return 0;
+} 
+
+// End of program
